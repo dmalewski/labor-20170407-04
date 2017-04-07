cd994caf61fe02efa692a96684f3639a95464116
Diana Malewski <talim08@hotmail.de>
2017-04-07 14:13:40 +0200

    verändert app.js multiply.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+//mtpl = Funktionsname
 const mtpl = require("./multiply");
 
-console.log(`Ergebnis ist: ${mtpl}`);
+//statt multiply nun "mtpl" schreiben!!
+const result = mtpl(2,5);
+
+console.log(`Ergebnis ist: ${result}`);
